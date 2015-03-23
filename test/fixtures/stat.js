dc50eb6005575543760cba0630b15a554cf6eb21
Mathias Buus <mathiasbuus@gmail.com>
2015-03-23 15:42:45 +0100

    add link tests

--- test/fixtures/stat.js
+++ test/fixtures/stat.js
@@ -4,7 +4,7 @@ module.exports = function (st) {
     atime: st.atime || new Date(),
     ctime: st.ctime || new Date(),
     size: st.size !== undefined ? st.size : 0,
-    mode: st.mode === 'dir' ? 16877 : (st.mode === 'file' ? 33188 : st.mode),
+    mode: st.mode === 'dir' ? 16877 : (st.mode === 'file' ? 33188 : (st.mode === 'link' ? 41453 : st.mode)),
     uid: st.uid !== undefined ? st.uid : process.getuid(),
     gid: st.gid !== undefined ? st.gid : process.getgid()
   }
